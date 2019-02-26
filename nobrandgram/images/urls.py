93c516c6d5001115d9115586c20cdf24239b8812
JoIreh <kingslanding@naver.com>
2019-02-26 17:47:33 +0900

    Commenting on an Image

--- nobrandgram/images/urls.py
+++ nobrandgram/images/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:id>/like/", view=views.LikeImage.as_view(), name='like_image')
+    path("<int:id>/like/", view=views.LikeImage.as_view(), name='like_image'),
+    path("<int:id>/comment/", view=views.CommentOnImage.as_view(), name='comment_image')
 ]
 
 #/images/3/like/
--- nobrandgram/images/views.py
+++ nobrandgram/images/views.py
@@ -31,7 +31,7 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, id, format=None):
+    def post(self, request, id, format=None):
 
         user = request.user
 
@@ -59,3 +59,26 @@ class LikeImage(APIView):
             new_like.save()
 
             return Response(status=status.HTTP_201_CREATED)
+
+class CommentOnImage(APIView):
+
+    def post(self, request, id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user, image=found_image)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
