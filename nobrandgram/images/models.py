9499c519dac7f2d0d480d4c5ff9f6eb35240d243
JoIreh <kingslanding@naver.com>
2019-02-26 15:53:55 +0900

    Getting the User Feed

--- nobrandgram/images/models.py
+++ nobrandgram/images/models.py
@@ -15,14 +15,20 @@ class TimeStampedModel(models.Model):
 class Image(TimeStampedModel):
 
     """ Image Model """
-
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name='images')
+
+    @property
+    def like_count(self):
+        return self.likes.all().count()
 
     def __str__(self):
         return '{} - {}'.format(self.location, self.caption)
+
+    class Meta:
+        ordering = ['-created_at']
 
 @python_2_unicode_compatible
 class Comment(TimeStampedModel):
@@ -31,7 +37,7 @@ class Comment(TimeStampedModel):
 
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='comments')
 
     def __str__(self):
         return self.message
@@ -42,7 +48,7 @@ class Like(TimeStampedModel):
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='likes')
 
     def __str__(self):
         return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
--- nobrandgram/images/views.py
+++ nobrandgram/images/views.py
@@ -2,32 +2,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
-class ListAllImages(APIView):
+class Feed(APIView):
 
     def get(self, request, format=None):
 
-        all_images = models.Image.objects.all()
+        user = request.user
 
-        serializer = serializers.ImageSerializer(all_images, many=True)
+        following_users = user.following.all()
 
-        return Response(data=serializer.data)
+        print(following_users)
 
-class ListAllComments(APIView):
+        image_list = []
 
-    def get(self, request, format=None):
+        for following_user in following_users:
 
-        all_comments = models.Comment.objects.all()
+            user_images = following_user.images.all()[:2]
 
-        serializer = serializers.CommentSerializer(all_comments, many=True)
+            for image in user_images:
 
-        return Response(data=serializer.data)
+                image_list.append(image)
 
-class ListAllLikes(APIView):
+        sorted_list = sorted(image_list, key=lambda x: x.created_at, reverse=True)
 
-    def get(self, request, format=None):
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
 
-        all_likes = models.Like.objects.all()
-
-        serializer = serializers.LikeSerializer(all_likes, many=True)
-
-        return Response(data=serializer.data)
+        return Response(serializer.data)
