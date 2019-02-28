e6d0df187e3528072351b332b34057d0a80cffd3
JoIreh <kingslanding@naver.com>
2019-02-28 10:18:11 +0900

    Backend done

--- nobrandgram/users/urls.py
+++ nobrandgram/users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("search/", view=views.Search.as_view(), name='search'),
     path("<str:username>/", view=views.UserProfile.as_view(), name='user_profile'),
     path("<str:username>/password", view=views.ChangePassword.as_view(), name='change'),
+    path("login/facebook/", view=views.FacebookLogin.as_view(), name='fb_login'),
 ]
